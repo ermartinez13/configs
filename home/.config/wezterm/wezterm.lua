7a02c5b36ee094db8f956a335976f0fd1c145a77
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2026-09-23 12:11:20 -0500

    enable option + arrow for word navigation

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 config.color_scheme = "rose-pine-moon"
 config.font_size = 15.0
@@ -8,5 +9,20 @@ config.window_background_opacity = 0.8
 config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+
+config.keys = {
+  -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match
+  -- Terminal.app behavior
+  {
+    key = "LeftArrow",
+    mods = "OPT",
+    action = act.SendKey({ key = "b", mods = "ALT" }),
+  },
+  {
+    key = "RightArrow",
+    mods = "OPT",
+    action = act.SendKey({ key = "f", mods = "ALT" }),
+  },
+}
 
 return config
